3a003571d71a446d3dea84b6ea4f28e2252b697c
KeionT <93876343+KeionT@users.noreply.github.com>
2021-12-12 20:06:24 -0500

    1st Commit

---
+++ Week3HW2FunctionsQ2a.js
@@ -0,0 +1,20 @@
+//let fahrenheitPrompt = prompt("Enter Fahrenheit: ")
+
+// function fahrenheitToCelsius(){
+//     let output = celsius * 9/5 + 32
+//     return fahrenheit
+// }
+// fahrenheitToCelsius(7)
+// //subtract 32 and multiply by . 5556
+
+
+//console.log(input)
+const celsiusToFahrenheit = celsius => celsius * 9/5 + 32;
+
+//const fahrenheitToCelsius = fahrenheit => (fahrenheit - 32) * 5/9;
+
+
+celsiusToFahrenheit(0);   
+//fahrenheitToCelsius(59); 
+console.log(celsiusToFahrenheit(44))
+//console.log(fahrenheitToCelsius(44))
---
+++ Week3HW2Functionsq4.js
@@ -0,0 +1,8 @@
+const numsArray = [2,3,7,10,45]
+//chaining wtih a annonmous 
+const newNumbsArray = numsArray.map((num) => { //singular
+return num += 2
+})
+
+console.log(newNumbsArray)
+//console.log(numsArray)
